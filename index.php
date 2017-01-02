01acfb9a224ed2888e26ada3f11ac672c6abbe4d
Saad Ismail <me@saadismail.net>
2017-01-02 16:27:21 +0500

    First commit

---
+++ index.php
@@ -0,0 +1,193 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+	    <meta charset="utf-8">
+	    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+	    <meta name="viewport" content="width=device-width, initial-scale=1">
+	    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+	    <meta name="description" content="">
+	    <meta name="author" content="">
+	    <link rel="icon" href="img/favicon.ico">
+
+		<title>Saad Ismail - A Tech Savvy</title>
+
+	    <!-- Bootstrap core CSS -->
+	    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+	    <!-- Custom styles for this template -->
+	    <link href="starter-template.css" rel="stylesheet">
+
+	    <!-- Font Awesome -->
+	    <link rel="stylesheet" href="fonts/css/font-awesome.min.css">
+
+	    <!-- Custom CSS rules by Saad -->
+	    <link href="css/navbar.css" rel="stylesheet">
+	    <link href="css/custom.css" rel="stylesheet">
+	    <link href='https://fonts.googleapis.com/css?family=Lobster' rel='stylesheet' type='text/css'>
+		<link href='https://fonts.googleapis.com/css?family=Arvo' rel='stylesheet' type='text/css'>
+
+	    <!-- Just for debugging purposes. Don't actually copy these 2 lines! -->
+	    <!--[if lt IE 9]><script src="../../assets/js/ie8-responsive-file-warning.js"></script><![endif]-->
+	    <script src="js/ie-emulation-modes-warning.js"></script>
+
+	    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+	    <!--[if lt IE 9]>
+	      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+	      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+	    <![endif]-->
+	    <link rel="icon" href="img/favicon.png" type="image/png" sizes="16x16">
+
+	</head>
+	<body>
+	    <nav class="navbar navbar-default navbar-fixed-top">
+	      <div class="container">
+	        <div class="navbar-header">
+	          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+	            <span class="sr-only">Toggle navigation</span>
+	            <span class="icon-bar"></span>
+	            <span class="icon-bar"></span>
+	            <span class="icon-bar"></span>
+	          </button>
+	          <a class="navbar-brand" href="#"><img src="img/top_logo.png"></a>
+	        </div>
+	        <div id="navbar" class="collapse navbar-collapse">
+	          <ul class="nav navbar-nav navbar-right">
+	            <li class="active"><a href="#">Home</a></li>
+	            <li><a href="https://www.facebook.com/saaadyi" target="_blank">Facebook</a></li>
+	            <li><a href="https://github.com/saadismail/" target="_blank">Github</a></li>
+	            <li><a href="mailto:me@saadismail.net" target="_blank">Email</a></li>
+	          </ul>
+	        </div><!--/.nav-collapse -->
+	      </div>
+	    </nav>
+	    <header>
+		    <div class="container">
+		    	<img class="img-responsive img-circle" src="img/1x1.jpg" alt="Saad Ismail's Pic">
+		    	<h1>Hi! I'm <b>Saad Ismail</b></h1>
+		    	<h3>A passionate, enthusiastic & tech savvy always looking to improve his skills</h3>
+		   	</div>
+	   	</header>
+
+	   	<!--Skills Section -->
+	   	<div id="skills">
+	   	<div class="container">
+	   		<div class="row">
+
+	   			<h2>My Roles</h2>
+	   			<hr>
+	   			<div class="col-lg-3"><span class="fa-stack fa-lg fa-5x">
+					<i class="fa fa-circle fa-stack-2x"></i>
+					<i class="fa fa-book fa-stack-1x fa-inverse"></i>
+					</span>
+					<h3>Student</h3>
+					<p>Acc. to my teachers, I'm a good learner. But most of what I learn doesn't include any kind of teacher</p>
+				</div>
+	   			<div class="col-lg-3"><span class="fa-stack fa-lg fa-5x">
+					<i class="fa fa-circle fa-stack-2x"></i>
+					<i class="fa fa-support fa-stack-1x fa-inverse"></i>
+					</span>
+					<h3>Support Guy</h3>
+					<p>I work as a full-time support guy for web hosting projects where I love to help peoples getting out of troubles.</p>
+				</div>
+	   			<div class="col-lg-3"><span class="fa-stack fa-lg fa-5x">
+					<i class="fa fa-circle fa-stack-2x"></i>
+					<i class="fa fa-code fa-stack-1x fa-inverse"></i>
+					</span>
+					<h3>Coder</h3>
+					<p>I ain't a real coder yet & I'm still in learning stage. I often write bash scripts to automate linux processes.</p>
+				</div>
+	   			<div class="col-lg-3"><span class="fa-stack fa-lg fa-5x">
+					<i class="fa fa-circle fa-stack-2x"></i>
+					<i class="fa fa-server fa-stack-1x fa-inverse"></i>
+					</span>
+					<h3>Linux Admin.</h3>
+					<p>I administer tens of linux servers for various individuals as well as organizations.
+				</div>
+	   		</div>
+	   	</div>
+	   	</div>
+	   	<!-- End of Skills Section -->
+
+	   	<!-- Expertise Section -->
+	   	<div id="expertise">
+	   		<div class="container">
+	   			<h2>Expertise</h2>
+	   			<hr>
+				<h4>Wordpress</h4>
+				<div class="progress">
+				<div class="progress-bar progress-bar-striped progress-bar-info active" role="progressbar" aria-valuenow="80" aria-valuemin="0" aria-valuemax="100" style="width: 80%">
+					<b>80%</b>
+				</div>
+				</div>
+				<h4>Linux Administration</h4>
+				<div class="progress">
+				<div class="progress-bar progress-bar-striped progress-bar-info active" role="progressbar" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100" style="width: 75%">
+				<b>75%</b>
+				</div>
+				</div>
+				<h4>Bash</h4>
+				<div class="progress">
+				<div class="progress-bar progress-bar-striped progress-bar-info active" role="progressbar" aria-valuenow="80" aria-valuemin="0" aria-valuemax="100" style="width: 80%">
+				<b>80%</b>
+				</div>
+				</div>
+				<h4>HTML/CSS</h4>
+				<div class="progress">
+				<div class="progress-bar progress-bar-striped progress-bar-info active" role="progressbar" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100" style="width: 60%">
+				<b>60%</b>
+				</div>
+				</div>
+	   		</div>
+	   	</div>
+	   	<!-- End of Expertise Section -->
+
+	   	<!-- Contact Section -->
+	   	<div id="contact">
+	   		<div class="container">
+	   			<div class="row">
+	   				<h2>Contact Me</h2>
+	   				<hr>
+	   				<div class="col-lg-4">
+	   					<span class="fa-stack fa-lg fa-3x">
+							<i class="fa fa-circle fa-stack-2x"></i>
+							<i class="fa fa-envelope fa-stack-1x fa-inverse"></i>
+						</span>
+	   					<h3>E-Mail</h3>
+	   					<p>Get the fastest response from me through E-Mail: <a href="mailto:me@saadismail.net" target="_blank">me@saadismail.net</a>
+	   				</div>
+	   				<div class="col-lg-4">
+	   					<span class="fa-stack fa-lg fa-3x">
+							<i class="fa fa-circle fa-stack-2x"></i>
+							<i class="fa fa-skype fa-stack-1x fa-inverse"></i>
+						</span>
+	   					<h3>Skype</h3>
+	   					<p>You can also reach me through skype in day time: <a href="skype:sismail3112?add" target="_blank">SIsmail3112</a>
+	   				</div>
+	   				<div class="col-lg-4">
+	   					<span class="fa-stack fa-lg fa-3x">
+							<i class="fa fa-circle fa-stack-2x"></i>
+							<i class="fa fa-phone fa-stack-1x fa-inverse"></i>
+						</span>
+	   					<h3>Phone</h3>
+	   					<p>I can be available over phone too, request my digits: <a href="mailto:me@saadismail.net?subject=I need your contact number" target="_blank">My Phone Digits</a>
+	   				</div>
+	   			</div>
+	   		</div>
+	   	</div>
+	   	<!-- End of Contact Section -->
+
+	   	<!-- Footer Section -->
+	   	<footer>
+	   		<p><b>Copyright &copy; Saad Ismail <?php echo date('Y'); ?></b></p>
+	   	</footer>
+
+	    <!-- Bootstrap core JavaScript
+	    ================================================== -->
+	    <!-- Placed at the end of the document so the pages load faster -->
+	    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+	    <script src="../js/bootstrap.min.js"></script>
+	    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+	    <script src="js/ie10-viewport-bug-workaround.js"></script>
+
+	</body>
+</html>
